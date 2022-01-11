3492b770bbc482a78eb4548b2f91518f68df34f2
Aasis <aasispasa@gmail.com>
2022-01-11 18:03:31 +0545

    Cable arranged

--- Cross.py
+++ Cross.py
@@ -2,6 +2,7 @@ import pandas as pd
 import csv
 from reaction import bm_udl
 from irc6_2007 import ped_ll
+import math
 
 # import tkinter as tk
 # from PIL import Image,ImageTk
@@ -130,20 +131,40 @@ def stiffness(fck,I,l):
 
 
 class Cross_section:
-    def __init__(self,length,height):
+    def __init__(self,length,height,span,section_at):
         self.length=length
         self.height=height
+        self.span=span
+        self.section_at=section_at
+        if self.expansion_width>0:
+            if self.expansion_width>=self.height[10] and self.expansion_width>=self.height[11]:
+                h1=self.height[10]
+                h2=self.height[11]
+            else:
+                h1=h2=0
+            self.length=self.length+[self.length[10],round(self.length[2]*0.45-self.length[10]*2,5),round(self.length[2]*0.45-self.length[10],5),self.length[10],self.length[11],round(self.length[2]*0.45-self.length[11],5),round(self.length[2]*0.45-self.length[11]*2,5),self.length[11]]
+            self.height=self.height+[h1,h1,self.expansion_width-h1,self.expansion_width-h1,self.expansion_width-h2,self.expansion_width-h2,h2,h2]
+    
+    @property
+    def expansion_width(self):
+        return 0.5
 
     @property
     def name(self):
-        return['left Pillar','right pillar','bottom slab','top slab','left cantilever','right cantilever',
+        nyam=['left Pillar','right pillar','bottom slab','top slab','left cantilever','right cantilever',
       'left triangle','right triangle','left top fillet','right top fillet','left bottom fillet','right bottom fillet']
-        
+        if self.expansion_width>0:
+            nyam=nyam+['Expanded_triangle_left','Top_Expanded_rectangle_left','Side_Expanded_rectangle_left','Fillet_triangle_left','Fillet_triangle_right','Side_Expanded_rectangle_right',
+            'Top_Expanded_rectangle_right','Expanded_triangle_right']
+        return nyam
     
     @property
     def obj(self):    
-        return['rectangle','rectangle','rectangle','rectangle','rectangle','rectangle','triangle_1',
+        obui=['rectangle','rectangle','rectangle','rectangle','rectangle','rectangle','triangle_1',
      'triangle_3','triangle_3','triangle_1','triangle_2','triangle_4']
+        if self.expansion_width>0:
+            obui=obui+['triangle_1','rectangle','rectangle','triangle_2','triangle_4','rectangle','rectangle','triangle_3']
+        return obui
 
                   #
                   # \--------|                  |\                  |--------/                /|
@@ -160,20 +181,32 @@ class Cross_section:
     
     @property
     def position(self):
-        pos=[[3.5,3.5]]#left pillar
-        pos.append([pos[0][0]+self.length[0]+self.length[2],pos[0][1]])#right pillar
-        pos.append([pos[0][0]+self.length[0],pos[0][1]])#bottom slab
-        pos.append([pos[0][0]+self.length[0],pos[0][1]+self.height[0]-self.height[3]])#top slab
-        pos.append([round(pos[0][0]-self.length[4],4),round(pos[0][1]+self.height[0]-self.height[4],4)])#left cantilever
-        pos.append([round(pos[0][0]+self.length[0]+self.length[3]+self.length[1],4),round(pos[0][1]+self.height[0]-self.height[5],4)])#right cantilever
-        # pos.append([round(pos[0][0]-length[4],4),round(pos[0][1]+height[0],4)])#left kerb
+        pos=[[3.5,3.5]]#left pillar  0
+        pos.append([pos[0][0]+self.length[0]+self.length[2],pos[0][1]])#right pillar  1
+        pos.append([pos[0][0]+self.length[0],pos[0][1]])#bottom slab  2
+        pos.append([pos[0][0]+self.length[0],pos[0][1]+self.height[0]-self.height[3]])#top slab  3
+        pos.append([round(pos[0][0]-self.length[4],4),round(pos[0][1]+self.height[0]-self.height[4],4)])#left cantilever  4
+        pos.append([round(pos[0][0]+self.length[0]+self.length[3]+self.length[1],4),round(pos[0][1]+self.height[0]-self.height[5],4)])#right cantilever  5
+        # pos.append([round(pos[0][0]-length[4],4),round(pos[0][1]+height[0],4)])#left kerb  
         # pos.append([round(pos[0][0]+length[0]+length[3]+length[1]+length[5]-length[7],4),round(pos[0][1]+height[0],4)])#right kerb
-        pos.append([round(pos[0][0]-self.length[4],4),round(pos[0][1]+self.height[0]-self.height[4]-self.height[6],4)])#left triangle
-        pos.append([round(pos[0][0]+self.length[0]+self.length[3]+self.length[1],4),round(pos[0][1]+self.height[0]-self.height[5]-self.height[7],4)])#right triangle
-        pos.append([round(pos[0][0]+self.length[0],4),round(pos[0][1]+self.height[0]-self.height[3]-self.height[9],4)])#left top fillet
-        pos.append([round(pos[0][0]+self.length[0]+self.length[3]-self.length[10],4),round(pos[0][1]+self.height[0]-self.height[3]-self.height[9],4)])#right top fillet
-        pos.append([round(pos[0][0]+self.length[0],4),round(pos[0][1]+self.height[2],4)])#left bottom fillet
-        pos.append([round(pos[0][0]+self.length[0]+self.length[2]-self.length[11],4),round(pos[0][1]+self.height[2],4)])#right bottom fillet
+        pos.append([round(pos[0][0]-self.length[4],4),round(pos[0][1]+self.height[0]-self.height[4]-self.height[6],4)])#left triangle  6
+        pos.append([round(pos[0][0]+self.length[0]+self.length[3]+self.length[1],4),round(pos[0][1]+self.height[0]-self.height[5]-self.height[7],4)])#right triangle  7
+        pos.append([round(pos[0][0]+self.length[0],4),round(pos[0][1]+self.height[0]-self.height[3]-self.height[9],4)])#left top fillet  8
+        pos.append([round(pos[0][0]+self.length[0]+self.length[3]-self.length[10],4),round(pos[0][1]+self.height[0]-self.height[3]-self.height[9],4)])#right top fillet  9
+        pos.append([round(pos[0][0]+self.length[0],4),round(pos[0][1]+self.height[2],4)])#left bottom fillet  10
+        pos.append([round(pos[0][0]+self.length[0]+self.length[2]-self.length[11],4),round(pos[0][1]+self.height[2],4)])#right bottom fillet  11
+        if self.expansion_width>0:
+            pos=pos+[[pos[10][0],pos[10][1]]]
+            pos = pos+ [[pos[10][0]+self.length[10],pos[10][1]]]
+            pos= pos + [[pos[10][0],pos[10][1]+self.height[10]]]
+            pos = pos +[[round(pos[10][0]+self.length[2]*0.45-self.length[10],4),pos[10][1]]]
+
+            
+            pos = pos+ [[pos[11][0]-self.length[2]*0.45-self.length[11],pos[11][1]]]
+            pos = pos+[[pos[11][0]+self.length[11]-self.length[2]*0.45,pos[11][1]+self.height[11]]]
+            pos = pos+ [[pos[11][0]-self.length[2]*0.45+self.length[11],pos[11][1]]]
+            pos=pos+[[pos[11][0],pos[11][1]]]
+
         return pos
     
     @property
@@ -205,7 +238,120 @@ class Cross_section:
         for i in range(len(self.obj)):
             moi.append(calc_moi(self.obj[i],self.dimensions[i]))
         return moi
+
+    def cable_pos(self,f,nos):
+
+        fck=35
+        if fck<45:
+            mina=0.280+(45-fck)*4/1000
+            minb=0.200+(45-fck)*4/1000
+
+        else:
+            mina=0.280
+            minb=0.220
+
+        expanlen=min(section.length[13],section.length[16])
+        b=section.height[2]/2
+        if b<minb:
+            raise ValueError ("Very bad input section")
+        a=0.450
+        if a<mina:
+            raise ValueError ("Very bad input section")
+        e=section.length[0]/2
+        if e<=b:
+            raise ValueError ("Very bad input section")
+
+        cc=round(section.position[1][0]+section.length[1]-section.position[0][0]-section.length[0],3)
+
+        n=14
+
+        ntop=round(n/4)/2
+        nbot=n-ntop*2
+
+
+    
+        nbotup=0
+
+        adash=cc/(nbot+1)
+        while adash<a:
+
+            if (nbotup/2)<=math.floor((expanlen-2*e)/a):
+                nbot=nbot-2
+                nbotup=nbotup+2
+                adash=cc/(nbot+1)
+            else:
+                ntop=ntop+1
+                nbot=nbot-2
+                adash=cc/(nbot+1)
+
+        nbotup=nbotup/2
+        amid=0.15
+        expanlen
+
+        ####################################################
+
+        toppos=[]
+        midtop=[]
+        bottompos=[]
+        midbot=[]
+        bottomposupleft=[]
+        bottomposupright=[]
+        midbotupleft=[]
+        midbotupright=[]
+        ntop=int(ntop)
+        nbot=int(nbot)
+        nbotup=int(nbotup)
+        for i in range(ntop):
+            if i==0:
+                toppos.append([round(x+y,4) for x,y in zip(section.position[0],[e,b+3*a])])
+                midtop.append([round(x+y,4) for x,y in zip(section.position[0],[e,b+3*amid])])
+                
+            if i>0:
+                toppos.append([round(x+y,4) for x,y in zip(toppos[i-1],[0,a])])
+                midtop.append([round(x+y,4) for x,y in zip(midtop[i-1],[0,amid])])
+
+        for i in range(len(toppos)):
+            toppos.append([x+y for x,y in zip(toppos[i],[cc,0])])
+            midtop.append([x+y for x,y in zip(midtop[i],[cc,0])])
+
+            
+        for i in range(nbot):
+            if i==0:
+                bottompos.append([round(x+y,4) for x,y in zip(section.position[0],[e+adash,b])])
+                midbot.append([round(x+y,4) for x,y in zip(section.position[0],[e+adash,b])])
+            if i>0:
+                bottompos.append([round(x+y,4) for x,y in zip(bottompos[i-1],[adash,0])])
+                midbot.append([round(x+y,4) for x,y in zip(midbot[i-1],[adash,0])])
+        for i in range(nbotup):
+            if i==0:
+                bottomposupleft.append([round(x+y,4) for x,y in zip(section.position[0],[e+adash,b+a])])
+                bottomposupright.append([round(x+y,4) for x,y in zip(section.position[1],[-e-adash,b+a])])
+                midbotupleft.append([round(x+y,4) for x,y in zip(section.position[0],[e+adash,b+a])])
+                midbotupright.append([round(x+y,4) for x,y in zip(section.position[0],[-e-adash,b+a])])
+            if i>0:
+                bottomposupleft.append([round(x+y,4) for x,y in zip(bottomposupleft[i-1],[a,0])])
+                bottomposupright.append([round(x+y,4) for x,y in zip(bottomposupright[i-1],[-a,0])])
+                midbotupleft.append([round(x+y,4) for x,y in zip(midbotupleft[i-1],[a,0])])
+                midbotupright.append([round(x+y,4) for x,y in zip(midbotupright[i-1],[-a,0])])
+        endcablepos=[*toppos,*bottompos,*bottomposupleft,*bottomposupright]
+
+        midcablepos=[*midtop,*midbot,*midbotupleft,*midbotupright]
+
+        x=self.section_at
+        l=self.span
+        cablepos=[]
+
+
+        for i in range(len(endcablepos)):
+            
+            h=midcablepos[i][1]-endcablepos[i][1]
+            pos= -((4*h*x**2)/(l**2))+4*h*x/l+endcablepos[i][1]
+            cablepos.append([endcablepos[i][0],pos])
         
+        return cablepos
+
+ 
+
 
     @property
     def Centroid(self):
@@ -337,7 +483,8 @@ def excel_loads(PDL,ODL,PEDL,SIDL,sc):
 
 
 
-section=Cross_section(length,height)
+section=Cross_section(length,height,50,25)
+
 PDL=Dead_Load('PDL',area_sum,sc,span,l_kerblen+r_kerblen,0.3,0.065,6,2,section.I[0],section.ymax,section.ymin)
 ODL=Dead_Load('ODL',area_sum,sc,span,l_kerblen+r_kerblen,0.3,0.065,6,2,section.I[0],section.ymax,section.ymin)
 PEDL=Dead_Load('PEDL',area_sum,sc,span,l_kerblen+r_kerblen,0.3,0.065,6,2,section.I[0],section.ymax,section.ymin)
@@ -345,3 +492,5 @@ SIDL=Dead_Load('SIDL',area_sum,sc,span,l_kerblen+r_kerblen,0.3,0.065,6,2,section
 # excel_export(section)
 # excel_loads(PDL,ODL,PEDL,SIDL,sc)
 # print(section.Centroid)
+
+print(section.cable_pos(35,14))
